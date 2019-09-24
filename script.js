4e8bd523484ae68854531e6c15c3caebec3001a2
Dee Lynn <deannalguzman@gmail.com>
2019-09-24 09:46:13 -0700

    added a slider

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-var alphnum = "abcdefghijklmnopqrstuvwxyz0123456789";
+var alphnum = ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&'()*+,-./:;<=>?@[]^_`{|}~");
 var randomIndex = Math.floor(Math.random() * alphnum.length);
       console.log("random index: ", randomIndex);
       console.log("random item: ", alphnum[randomIndex]);
 
+var alphnum = ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&'()*+,-./:;<=>?@[]^_`{|}~");
+var stringNum = 8;
+var password = "";
 
+    
+ 
 
-
-
-function clipboard(){
+function generatePass(){
+    
+    
+    for(var i = 0; i <= stringNum; i++){
+        
+    }
 
 }
