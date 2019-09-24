51621b00f4b6e683fd790a7e937788b27664258f
Dee Lynn <deannalguzman@gmail.com>
2019-09-23 18:13:08 -0700

    changed css for buttons

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-var alphnum = "abcdefghijklmnopqrstuvwxyz0123456789".split();
+var alphnum = "abcdefghijklmnopqrstuvwxyz0123456789";
+var randomIndex = Math.floor(Math.random() * alphnum.length);
+      console.log("random index: ", randomIndex);
+      console.log("random item: ", alphnum[randomIndex]);
