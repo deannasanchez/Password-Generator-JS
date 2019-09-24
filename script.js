a2324b1e66d9cb1707a54d473ee3abde1e779fcc
Dee Lynn <deannalguzman@gmail.com>
2019-09-23 20:30:34 -0700

    added bootstrap to edit the style of page. added more css elements to the page.

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ var alphnum = "abcdefghijklmnopqrstuvwxyz0123456789";
 var randomIndex = Math.floor(Math.random() * alphnum.length);
       console.log("random index: ", randomIndex);
       console.log("random item: ", alphnum[randomIndex]);
+
+
+
+
+
+function clipboard(){
+
+}
